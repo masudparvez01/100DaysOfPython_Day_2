a5e6fe4ed2007b1feff88c0d4dc533ec9591cf1c
masudparvez01 <masudparvez.me@gmail.com>
2023-04-25 16:19:01 +0600

    done

---
+++ code_challenge.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+first_char = int(two_digit_number[0])
+second_char = int(two_digit_number[1])
+sub_of_two_char = first_char + second_char
+
+print(sub_of_two_char)
